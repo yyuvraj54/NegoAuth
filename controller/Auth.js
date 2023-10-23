68dc9aa2bf30e182f5e7444bca564970947315f8
Yuvraj <yyuvraj54@gmail.com>
2023-10-23 11:29:11 +0530

    login Auth added

--- controller/Auth.js
+++ controller/Auth.js
@@ -1,6 +1,7 @@
 const bcrypt =require("bcrypt");
 const User=require("../models/User");
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
+const { undefined } = require("webidl-conversions");
 require("dotenv").config();
 
 exports.signup = async(req,res) => {
@@ -42,6 +43,72 @@ exports.signup = async(req,res) => {
             message:"User Alredy Exists" 
         });
     }
+}
+
+
+exports.login = async(req,res)=>{
+
+    try{
+        const {email, password} = req.body;
+        
+        if(!email || !password){
+            return res.status(400).json({
+                success:false,
+                message:"Please provide valid username or passsword"
+            });
+        }
+        var existingUser=await User.find({email});
+        existingUser=existingUser[0];
+        if(!existingUser){
+            return res.status(400).json({
+                Success:false,
+                message:"User not Found"
+            });
+        }
+
+        const payload ={
+            email:existingUser.email,
+            id:existingUser._id,
+        };
+
+        if(await bcrypt.compare(password,existingUser.password)){
+
+            let token = jwt.sign(payload,process.env.JWT_SECRET,{expiresIn:"2h"})
+
+            existingUser=existingUser.toObject();
+            existingUser.token=token;
+            existingUser.password=undefined;
+
+            const option={
+                expires:new Date(Date.now()+3*24*60*60*1000),
+                httpOnly:true,
+            }
+            res.cookie("cookie1",token,option).status(200).json({
+                success:"True",
+                token,
+                existingUser,
+                message:"User Logged In Successfully"
+            });
+
+        }
+        else{
+            return  res.status(403).json({
+                Success:"Failed",
+                message:"Password Incorrect"
+            });
+        }
+
+    }
+    catch(error){
+        console.error(error)
+        return  res.status(400).json({
+            Success:"Failed",
+            message:"something went wrong!"
+        });
+
+        
+    }
+    
 }
 
 
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,9 @@
 const express =require("express");
 const router= express.Router();
 
-const {signup} =require("../controller/Auth")
+const {signup,login} =require("../controller/Auth")
 
-// router.post("/login",login);
+router.post("/login",login);
 router.post("/signup",signup);
 
 module.exports= router;
